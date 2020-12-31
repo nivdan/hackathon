209a016422a8f8037c10d04a4ba8b3a7af92c227
Niv <nivdaniel99@gmail.com>
2020-12-31 11:06:54 +0200

    magic numbers removed

--- client.py
+++ client.py
@@ -8,6 +8,9 @@ import select
 import tty
 import termios
 from termcolor import colored
+# constants
+
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -19,73 +22,81 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-#function that check if any key is pressed
+
+serverPortUdp = 13117
+recvBufferSize1024 = 1024
+recvBufferSize2048 = 2048
+timeOutConstant = 10
+# function that check if any key is pressed
+
+
 def isData():
     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+
+
 old_settings = termios.tcgetattr(sys.stdin)
 
 clientName = "GoBackN\n"
 print(f"{bcolors.OKBLUE}Client started, listening for offer requests... {bcolors.ENDC}")
 
-#Main loop
+# Main loop
 while 1:
-    #Trying to receive server to connect to
-    serverPort = 13118
+    # Trying to receive server to connect to
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     serverSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-    serverSocket.bind(('', serverPort))
-    message, clientAddress = serverSocket.recvfrom(2048)
+    serverSocket.bind(('', serverPortUdp))
+    message, clientAddress = serverSocket.recvfrom(recvBufferSize2048)
     serverSocket.close()
-    #got message, clossing the socket and checking that the message is not corrupted
+    # got message, clossing the socket and checking that the message is not corrupted
     message = message.hex()
-    if (message[:8]!="feedbeef" or message[8:10]!= "02") :
+    if (message[:8] != "feedbeef" or message[8:10] != "02"):
         print(f"{bcolors.FAIL}message is corrupted{bcolors.ENDC}")
         continue
-    serverPort = int(message[10:],16)
+    serverPortTcp = int(message[10:], 16)
 
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    print(f"{bcolors.OKGREEN}Received offer from "+clientAddress[0]+", attempting to connect..."+f"{bcolors.ENDC}")
+    print(f"{bcolors.OKGREEN}Received offer from " +
+          clientAddress[0]+", attempting to connect..."+f"{bcolors.ENDC}")
     try:
-        #Trying to create tcp connection with the server
-        clientSocket.connect((clientAddress[0], int(serverPort)))
+        # Trying to create tcp connection with the server
+        clientSocket.connect((clientAddress[0], int(serverPortTcp)))
     except Exception as e:
-        #Throw exection if the server refuse the connection
+        # Throw exection if the server refuse the connection
         print(f"{bcolors.FAIL}"+str(e)+f"{bcolors.ENDC}")
         continue
     try:
-        #Trying to send the nick name to the server after the tcp connection established
+        # Trying to send the nick name to the server after the tcp connection established
         clientSocket.send(clientName.encode('utf-8'))
-        serverMsg = clientSocket.recv(1024)
+        serverMsg = clientSocket.recv(recvBufferSize1024)
     except Exception as e:
         print(f"{bcolors.FAIL}"+str(e)+f"{bcolors.ENDC}")
         continue
-    #Client recieve the start message
+    # Client recieve the start message
     print(f"{bcolors.OKCYAN}"+serverMsg.decode('utf-8')+f"{bcolors.ENDC}")
-    timeout = 10
     startTime = time.time()
     inp = None
-    #Loop to get keys from the client
+    # Loop to get keys from the client
     try:
         tty.setcbreak(sys.stdin.fileno())
         while 1:
             if isData():
-                inp=sys.stdin.read(1)
+                inp = sys.stdin.read(1)
                 try:
                     clientSocket.send(inp.encode("utf-8"))
                 except Exception as e:
                     print(f"{bcolors.FAIL}"+str(e)+f"{bcolors.ENDC}")
                     break
-            if time.time() - startTime > timeout:
+            if time.time() - startTime > timeOutConstant:
                 break
             time.sleep(0.01)
     finally:
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
     try:
-        gameOverMsg=clientSocket.recv(1024)
+        gameOverMsg = clientSocket.recv(recvBufferSize1024)
     except Exception as e:
         print(f"{bcolors.FAIL}"+str(e)+f"{bcolors.ENDC}")
         continue
-    #print the end message
+    # print the end message
     print(f"{bcolors.WARNING}"+gameOverMsg.decode('utf-8')+f"{bcolors.ENDC}")
     print("Server disconnected, listening for offer requests...")
--- server.py
+++ server.py
@@ -6,7 +6,8 @@ from _thread import *
 import random
 
 hostname = gethostname()
-serverName = gethostbyname(hostname) 
+serverName = gethostbyname(hostname)
+#constants
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -17,65 +18,80 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-#Thread that send the broadcast message in the udp chanel every second.
+
+offerCode ="02"
+serverPortTcp = 2133
+serverPortTcpHex = "0855"
+recvBufferSize=1024
+timeOutConstant=10
+serverPortUdp=13117
+
+# Thread that send the broadcast message in the udp chanel every second.
 def printit():
     # udp
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-    message = bytes.fromhex("feedbeef")+bytes.fromhex("02")+bytes.fromhex("0855")
+    message = bytes.fromhex("feedbeef") + \
+        bytes.fromhex(offerCode)+bytes.fromhex(serverPortTcpHex)
     threading.Timer(1.0, printit).start()
-    serverPort = 13118
-    clientSocket.sendto(message, ('<broadcast>', serverPort))
-#Thread that handle the messages from the client (thread per client)
+    clientSocket.sendto(message, ('<broadcast>', serverPortUdp))
+# Thread that handle the messages from the client (thread per client)
+
+
 def gameClientHandler(participantName):
     while gameFlag:
         try:
-            char=participants[participantName].recv(1024)
-            if char.decode('utf-8')=='':
+            char = participants[participantName].recv(recvBufferSize)
+            if char.decode('utf-8') == '':
                 break
             scores[participantName] += 1
         except:
             break
-#Thread that get the nick name from the client (thread per client)
+# Thread that get the nick name from the client (thread per client)
+
+
 def threaded(connectionSocket):
     try:
-        name = connectionSocket.recv(1024)
+        name = connectionSocket.recv(recvBufferSize)
         if name.decode('utf-8') in participants:
             print(f"{bcolors.FAIL}change your name{bcolors.ENDC}")
         participants[name.decode('utf-8')] = connectionSocket
     except Exception as e:
         print(f"{bcolors.FAIL}"+str(e)+f"{bcolors.ENDC}")
-#start to send udp broadcast messages
+
+
+# start to send udp broadcast messages
 printit()
-serverPortTcp = 2133
-print(f"{bcolors.OKBLUE}Server started, listening on IP address "+serverName+f"{bcolors.ENDC}")
-serverSocketTcp=None
-#Server main Loop
+
+print(f"{bcolors.OKBLUE}Server started, listening on IP address " +
+      serverName+f"{bcolors.ENDC}")
+serverSocketTcp = None
+# Server main Loop
 while 1:
-    #reset the dictionaries
+    # reset the dictionaries
     participants = {}
     scores = {}
     gameFlag = True
-    #Open socket to recieve connections for 10 seconds
+    # Open socket to recieve connections for 10 seconds
     serverSocketTcp = socket(AF_INET, SOCK_STREAM)  # tcp
     serverSocketTcp.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    serverSocketTcp.settimeout(10)  # timeout for listening
+    serverSocketTcp.settimeout(timeOutConstant)  # timeout for listening
     serverSocketTcp.bind(('', serverPortTcp))
     serverSocketTcp.listen(1)
-    #Loop that accepting the tcp connects
+    # Loop that accepting the tcp connects
     while 1:
         try:
             connectionSocket, addr = serverSocketTcp.accept()
             start_new_thread(threaded, (connectionSocket,))
         except:
             break
-    #10 seconds have passed now we divide the participants into to random groups
+    # 10 seconds have passed now we divide the participants into to random groups
     group1 = []
     group2 = []
-    participantsKeyList=list(participants.keys())
+    participantsKeyList = list(participants.keys())
     random.shuffle(participantsKeyList)
     for participantName in participants:
-        if participantsKeyList.index(participantName)<(len(participantsKeyList)/2):
+        if participantsKeyList.index(participantName) < (len(participantsKeyList)/2):
             group1.append(participantName)
         else:
             group2.append(participantName)
@@ -89,21 +105,21 @@ while 1:
         startGameMsg += participantName
 
     startGameMsg += 'Start pressing keys on your keyboard as fast as you can!!'
-    #Sending welcome message to all the participands
+    # Sending welcome message to all the participands
     for participantConnetSocket in participants.values():
         try:
             participantConnetSocket.send(
                 startGameMsg.encode('utf-8'))
         except Exception as e:
             print(f"{bcolors.FAIL}"+str(e)+f"{bcolors.ENDC}")
-    #stating thread to handle each of the participands
+    # stating thread to handle each of the participands
     for participantName in participants:
         scores[participantName] = 0
         start_new_thread(gameClientHandler, (participantName,))
-    #waiting until the game ends
-    time.sleep(10)
+    # waiting until the game ends
+    time.sleep(timeOutConstant)
     gameFlag = False
-    #calculating each group score
+    # calculating each group score
     group1Score = 0
     group2Score = 0
     for participantName in participants:
@@ -125,17 +141,16 @@ while 1:
             gameOverMsg += participantName
     else:
         gameOverMsg += "Its a tie!\n\n"+"There are no winners in this game!\n"
-    #sending game over message to all the participants
-    #print(gameOverMsg)
+    # sending game over message to all the participants
+    # print(gameOverMsg)
     for participantName in participants:
         try:
             participants[participantName].send(gameOverMsg.encode('utf-8'))
             participants[participantName].shutdown(SHUT_RDWR)
             participants[participantName].close()
-        except Exception as e :
+        except Exception as e:
             print(f"{bcolors.FAIL}"+str(e)+f"{bcolors.ENDC}")
     serverSocketTcp.shutdown(SHUT_RDWR)
     serverSocketTcp.close()
-    #Going back to offer request to clients
+    # Going back to offer request to clients
     print(f"{bcolors.WARNING}Game over, sending out offer requests...{bcolors.ENDC}")
-
